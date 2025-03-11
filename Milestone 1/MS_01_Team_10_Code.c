4922395fc9c483a798714df14e469feec3bc8433
JudyAlsofi <judyalsofi@gmail.com>
2025-03-11 16:32:13 +0200

    cpu utilization wrong

--- Milestone 1/MS_01_Team_10_Code.c
+++ Milestone 1/MS_01_Team_10_Code.c
@@ -21,10 +21,40 @@ typedef struct {
     struct rusage usage;
 } metrics_t;
 
+metrics_t metrics1, metrics2, metrics3;
+
+void calculate_metrics(metrics_t *metrics) {
+    metrics->execution_time = ((long double)(metrics->finish_time - metrics->start_time)) / CLOCKS_PER_SEC;
+    metrics->waiting_time = ((long double)(metrics->start_time - metrics->release_time)) / CLOCKS_PER_SEC;
+    metrics->response_time = metrics->waiting_time;
+    metrics->turnaround_time = ((long double)(metrics->finish_time - metrics->release_time)) / CLOCKS_PER_SEC;
+
+    struct timeval utime = metrics->usage.ru_utime;
+    struct timeval stime = metrics->usage.ru_stime;
+    
+    long double total_cpu_time = (utime.tv_sec + stime.tv_sec) + (utime.tv_usec + stime.tv_usec) / 1e6;
+    
+    metrics->cpu_utilization = (metrics->execution_time > 0) ? (total_cpu_time / metrics->execution_time) : 0.0;
+}
+
+void print_metrics(metrics_t *metrics, const char* thread_name, const char* policy_name) {
+    printf("\n%s with %s Scheduling:\n", thread_name, policy_name);
+    printf("Execution Time: %Lf seconds\n", metrics->execution_time);
+    printf("Release Time: %ld\n", metrics->release_time);
+    printf("Start Time: %ld\n", metrics->start_time);
+    printf("Finish Time: %ld\n", metrics->finish_time);
+    printf("Waiting Time: %Lf seconds\n", metrics->waiting_time);
+    printf("Response Time: %Lf seconds\n", metrics->response_time);
+    printf("Turnaround Time: %Lf seconds\n", metrics->turnaround_time);
+    printf("CPU Utilization: %Lf\n", metrics->cpu_utilization);
+    printf("Memory Consumption: %ld KB\n", metrics->usage.ru_maxrss);
+}
+
 void *thread1(void *args) {
+    metrics1.start_time = clock();
     char c1, c2;
     printf("Enter two alphabetical characters: ");
-    fflush(stdout); // Ensure the prompt is displayed immediately
+    fflush(stdout); 
     scanf(" %c %c", &c1, &c2);
     if (c1 > c2) {
         char temp = c1;
@@ -35,135 +65,96 @@ void *thread1(void *args) {
         printf("%c ", c);
     }
     printf("\n");
+    metrics1.finish_time = clock();
+    getrusage(RUSAGE_THREAD, &metrics1.usage);
+    calculate_metrics(&metrics1);
     pthread_exit(NULL);
 }
 
 void *thread2(void* arg) {
+    metrics2.start_time = clock();
     pthread_t thread_id = pthread_self();
-    printf("Thread 2 ID: %lu | : Thread is starting.\n", thread_id);
-    printf("Thread 2 ID: %lu | : Thread is running.\n", thread_id);
-    printf("Thread 2 ID: %lu | : Thread is ending.\n", thread_id);
+    printf("Thread 2 ID: %lu | Thread is running.\n", thread_id);
+    metrics2.finish_time = clock();
+    getrusage(RUSAGE_THREAD, &metrics2.usage);
+    calculate_metrics(&metrics2);
     return NULL;
 }
 
 void *thread3(void *args) {
-    int x, y, sum, count, prod;
+    metrics3.start_time = clock();
+    int x, y, sum = 0, prod = 1, count;
     float avg;
-    printf("Insert the first number: \n");
+    printf("Insert the first number: ");
     scanf("%d", &x);
-    printf("Insert the second number: \n");
+    printf("Insert the second number: ");
     scanf("%d", &y);
 
-    sum = 0;
-    prod = 1;
-    if (x == y) {
-        sum = x;
-        prod = x;
-        avg = x;
-    } else if (x < y) {
-        count = (y - x) + 1;
-        for (int i = x; i <= y; i++) {
-            sum += i;
-            prod *= i;
-        }
-    } else {
-        count = (x - y) + 1;
-        for (int i = y; i <= x; i++) {
-            sum += i;
-            prod *= i;
-        }
+    count = (x < y) ? (y - x) + 1 : (x - y) + 1;
+    for (int i = (x < y ? x : y); i <= (x > y ? x : y); i++) {
+        sum += i;
+        prod *= i;
     }
     avg = (float)sum / count;
-    printf("Thread 3\nSum = %d, Average = %.2f, Product = %d\n", sum, avg, prod);
+    printf("Thread 3: Sum = %d, Average = %.2f, Product = %d\n", sum, avg, prod);
+    
+    metrics3.finish_time = clock();
+    getrusage(RUSAGE_THREAD, &metrics3.usage);
+    calculate_metrics(&metrics3);
     pthread_exit(NULL);
 }
 
-void set_thread_scheduling(pthread_t thread, int policy, int priority) {
-    struct sched_param param;
-    param.sched_priority = priority;
-    pthread_setschedparam(thread, policy, &param);
-}
-
-void print_metrics(metrics_t metrics, const char* thread_name, const char* policy_name) {
-    metrics.execution_time = ((long double) (metrics.finish_time - metrics.start_time)) / CLOCKS_PER_SEC;
-    metrics.waiting_time = ((long double) (metrics.start_time - metrics.release_time)) / CLOCKS_PER_SEC;
-    metrics.response_time = metrics.waiting_time;
-    metrics.turnaround_time = ((long double) (metrics.finish_time - metrics.release_time)) / CLOCKS_PER_SEC;
-    if (metrics.execution_time > 0) {
-        metrics.cpu_utilization = ((long double) (metrics.usage.ru_utime.tv_sec + metrics.usage.ru_stime.tv_sec)) / metrics.execution_time;
-    } else {
-        metrics.cpu_utilization = 0.0;
-    }
-
-    printf("%s with %s Scheduling:\n", thread_name, policy_name);
-    printf("Execution Time: %Lf seconds\n", metrics.execution_time);
-    printf("Release Time: %ld\n", metrics.release_time);
-    printf("Start Time: %ld\n", metrics.start_time);
-    printf("Finish Time: %ld\n", metrics.finish_time);
-    printf("Waiting Time: %Lf seconds\n", metrics.waiting_time);
-    printf("Response Time: %Lf seconds\n", metrics.response_time);
-    printf("Turnaround Time: %Lf seconds\n", metrics.turnaround_time);
-    printf("CPU Utilization: %Lf\n", metrics.cpu_utilization);
-    printf("Memory Consumption: %ld KB\n", metrics.usage.ru_maxrss);
-}
-
-void run_thread_with_policy(pthread_t *thread, pthread_attr_t *attr, void *(*thread_func)(void *), int policy, int priority, const char* thread_name, const char* policy_name) {
-    struct sched_param param;
-    cpu_set_t cpuset;
-    metrics_t metrics;
-
-    // Set scheduling policy and priority
-    pthread_attr_setschedpolicy(attr, policy);
-    param.sched_priority = priority;
-    pthread_attr_setschedparam(attr, &param);
-
-    // Set CPU affinity (BEFORE thread starts)
-    CPU_ZERO(&cpuset);
-    CPU_SET(0, &cpuset); // Assigning the thread to CPU core 0
-
-    metrics.release_time = clock();
-    
-    pthread_create(thread, attr, thread_func, NULL);
-
-    // Apply CPU affinity to the created thread immediately
-    pthread_setaffinity_np(*thread, sizeof(cpu_set_t), &cpuset);
-    cpu_set_t check_set;
-    CPU_ZERO(&check_set);
-    pthread_getaffinity_np(*thread, sizeof(cpu_set_t), &check_set);
-    if (!CPU_ISSET(0, &check_set)) {
-        printf("CPU affinity was not set correctly!\n");
-    }
-
-    metrics.start_time = clock();
-    pthread_join(*thread, NULL);
-    metrics.finish_time = clock();
-    getrusage(RUSAGE_THREAD, &metrics.usage);
-    print_metrics(metrics, thread_name, policy_name);
-}
-
-
-
 int main(){
     pthread_t ptid1, ptid2, ptid3;
-    pthread_attr_t attr;
+    pthread_attr_t attr1, attr2, attr3;
+    struct sched_param param;
+    cpu_set_t cpuset;
 
-    pthread_attr_init(&attr);
-    pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+    CPU_ZERO(&cpuset);
+    CPU_SET(0, &cpuset);
+    sched_setaffinity(0, sizeof(cpu_set_t), &cpuset);
 
-    // Run each thread with FIFO, Round Robin, and SJF scheduling policies
-    // run_thread_with_policy(&ptid1, &attr, thread1, SCHED_FIFO, 90, "Thread 1", "FIFO");
-    run_thread_with_policy(&ptid1, &attr, thread1, SCHED_RR, 90, "Thread 1", "Round Robin");
-    // run_thread_with_policy(&ptid1, &attr, thread1, SCHED_OTHER, 0, "Thread 1", "SJF");
+    pthread_attr_init(&attr1);
+    pthread_attr_init(&attr2);
+    pthread_attr_init(&attr3);
+    
+    pthread_attr_setinheritsched(&attr1, PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setinheritsched(&attr2, PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setinheritsched(&attr3, PTHREAD_EXPLICIT_SCHED);
 
-    // run_thread_with_policy(&ptid2, &attr, thread2, SCHED_FIFO,50, "Thread 2", "FIFO");
-    run_thread_with_policy(&ptid2, &attr, thread2, SCHED_RR, 50, "Thread 2", "Round Robin");
-    // run_thread_with_policy(&ptid2, &attr, thread2, SCHED_OTHER, 0, "Thread 2", "SJF");
+    // Thread 1
+    metrics1.release_time = clock();
+    pthread_attr_setschedpolicy(&attr1, SCHED_RR);
+    param.sched_priority = 90;
+    pthread_attr_setschedparam(&attr1, &param);
+    pthread_create(&ptid1, &attr1, thread1, NULL);
 
-    // run_thread_with_policy(&ptid3, &attr, thread3, SCHED_FIFO, 10, "Thread 3", "FIFO");
-    run_thread_with_policy(&ptid3, &attr, thread3, SCHED_RR, 10, "Thread 3", "Round Robin");
-    // run_thread_with_policy(&ptid3, &attr, thread3, SCHED_OTHER, 0, "Thread 3", "SJF");
+    // Thread 2
+    metrics2.release_time = clock();
+    pthread_attr_setschedpolicy(&attr2, SCHED_RR);
+    param.sched_priority = 30;
+    pthread_attr_setschedparam(&attr2, &param);
+    pthread_create(&ptid2, &attr2, thread2, NULL);
 
-    pthread_attr_destroy(&attr);
+    // Thread 3
+    metrics3.release_time = clock();
+    pthread_attr_setschedpolicy(&attr3, SCHED_RR);
+    param.sched_priority = 10;
+    pthread_attr_setschedparam(&attr3, &param);
+    pthread_create(&ptid3, &attr3, thread3, NULL);
+
+    pthread_join(ptid1, NULL);
+    print_metrics(&metrics1, "Thread 1", "Round Robin");
+
+    pthread_join(ptid2, NULL);
+    print_metrics(&metrics2, "Thread 2", "Round Robin");
+
+    pthread_join(ptid3, NULL);
+    print_metrics(&metrics3, "Thread 3", "Round Robin");
+
+    pthread_attr_destroy(&attr1);
+    pthread_attr_destroy(&attr2);
+    pthread_attr_destroy(&attr3);
 
     return 0;
 }
